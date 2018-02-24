64d4b5ca5fa9dde65bba67a7cad1ae2916faa33b
Juan Morillo <juanmorillios@gmail.com>
2018-02-24 17:13:24 +0100

    Custom UITableViewCells

---
+++ ConferenceApp/SessionTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SessionTableViewCell.swift
+//  ConferenceApp
+//
+//  Created by Juan Morillo on 23/2/18.
+//  Copyright © 2018 Juan Morillo. All rights reserved.
+//
+
+import UIKit
+
+class SessionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
